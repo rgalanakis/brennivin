84099d998d8f7e89fcac4417c86a46f74abd0406
Rob Galanakis <rob.galanakis@gmail.com>
2013-12-31 19:07:15 -0600

    Added nicenum

---
+++ brennivin/nicenum.py
@@ -0,0 +1,83 @@
+"""
+Functions for formatting numbers in a pretty way.
+
+Members
+=======
+"""
+import math
+
+
+# noinspection PyShadowingBuiltins
+def format(num, precision):
+    """
+    Returns a string representation for a floating point number
+    that is rounded to the given precision and displayed with
+    commas and spaces.
+
+    >>> print format(123567.0, 1000)
+    124,000
+    >>> print format(5.3918e-07, 1e-10)
+    0.000 000 539 2
+
+    This kind of thing is wonderful for producing tables for
+    human consumption.
+    """
+    accpow = math.floor(math.log10(precision))
+    if num < 0:
+        digits = int(math.fabs(num / pow(10, accpow) - 0.5))
+    else:
+        digits = int(math.fabs(num / pow(10, accpow) + 0.5))
+    result = ''
+    if digits > 0:
+        for i in range(0, int(accpow)):
+            if (i % 3) == 0 and i > 0:
+                result = '0,' + result
+            else:
+                result = '0' + result
+        curpow = int(accpow)
+        while digits > 0:
+            adigit = chr((digits % 10) + ord('0'))
+            if (curpow % 3) == 0 and curpow != 0 and len(result) > 0:
+                if curpow < 0:
+                    result = adigit + ' ' + result
+                else:
+                    result = adigit + ',' + result
+            elif curpow == 0 and len(result) > 0:
+                result = adigit + '.' + result
+            else:
+                result = adigit + result
+            digits /= 10
+            curpow += 1
+        for i in range(curpow, 0):
+            if (i % 3) == 0 and i != 0:
+                result = '0 ' + result
+            else:
+                result = '0' + result
+        if curpow <= 0:
+            result = "0." + result
+        if num < 0:
+            result = '-' + result
+    else:
+        result = "0"
+    return result
+
+
+KB = 1024
+
+
+def format_memory(val):
+    """Pretty formatting of memory."""
+    val = float(val)
+    if val < KB:
+        label = "B"
+    elif KB < val < KB ** 2:
+        label = "KB"
+        val /= KB
+    elif KB ** 2 < val < KB ** 3:
+        label = "MB"
+        val /= KB ** 2
+    # elif val > KB**3:
+    else:
+        label = "GB"
+        val /= KB ** 3
+    return str(round(val, 2)) + label
---
+++ tests/test_nicenum.py
@@ -0,0 +1,45 @@
+import unittest
+
+from brennivin import nicenum as n
+
+
+class FormatTests(unittest.TestCase):
+
+    def assertResult(self, ideal, *args):
+        self.assertEqual(n.format(*args), ideal)
+
+    def testInt(self):
+        self.assertResult('2', 2, 3)
+        self.assertResult('0', 0, 3)
+        self.assertResult('23,456,789', 23456789, 3)
+        self.assertResult('-23,456,789', -23456789, 3)
+
+    def testFloat(self):
+        self.assertResult('124,000', 123567.0, 1000)
+        self.assertResult('120,000', 123567.0, 10000)
+        self.assertResult('123,567.0', 123567.0, 0.1)
+        self.assertResult('0.000 000 539 2', 5.3918e-07, 1e-10)
+
+    def testLong(self):
+        self.assertResult('123', 123L, 1)
+
+
+class FormatMemTests(unittest.TestCase):
+
+    def assertResult(self, ideal, val):
+        self.assertEqual(n.format_memory(val), ideal)
+
+    def testB(self):
+        self.assertResult('2.0B', 2)
+
+    def testKB(self):
+        self.assertResult('1.95KB', 2000)
+
+    def testMB(self):
+        self.assertResult('1.91MB', 2000000)
+
+    def testGB(self):
+        self.assertResult('1.86GB', 2000000000)
+
+    def testTB(self):
+        self.assertResult('1862.65GB', 2000000000000)
