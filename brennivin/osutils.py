4e231f965258a0554432ea1ab674dd63565c2cc6
Rob Galanakis <rob.galanakis@gmail.com>
2014-06-26 15:01:50 -0500

    Added needed functions.

--- brennivin/osutils.py
+++ brennivin/osutils.py
@@ -11,6 +11,14 @@ import fnmatch
 import os
 
 
+def crc_from_filename(filename):
+    """Returns the 32-bit crc for the file at filename."""
+    with open(filename, 'rb') as f:
+        data = f.read()
+    # See python docs for reason for &
+    return binascii.crc32(data) & 0xffffffff
+
+
 def iter_files(directory, pattern='*'):
     """Returns a generator of files under directory that match pattern."""
     for root, dirs, files in os.walk(directory):
@@ -20,9 +28,7 @@ def iter_files(directory, pattern='*'):
                 yield filename
 
 
-def crc_from_filename(filename):
-    """Returns the 32-bit crc for the file at filename."""
-    with open(filename, 'rb') as f:
-        data = f.read()
-    # See python docs for reason for &
-    return binascii.crc32(data) & 0xffffffff
+def listdirex(path, pattern='*.*'):
+    """Return absolute filepaths in ``path`` that matches ``pattern``."""
+    return [os.path.join(path, fn) for fn in os.listdir(path)
+            if fnmatch.fnmatch(fn, pattern)]
--- brennivin/testhelpers.py
+++ brennivin/testhelpers.py
@@ -5,6 +5,7 @@ Members
 =======
 """
 from __future__ import print_function
+import xml.etree.ElementTree as _elementtree
 
 from . import dochelpers as _dochelpers, osutils as _osutils
 
@@ -38,6 +39,64 @@ def assertCrcEqual(testcase, calcpath, idealpath, asLib=False):
     testcase.assertEqual(crc1, crc2,
                          'ideal: %(idealpath)s (%(crc2)s) !='
                          ' calc: %(calcpath)s (%(crc1)s)' % locals())
+
+
+def compareXml(x1, x2, reporter=_dochelpers.identity):
+    if x1.tag != x2.tag:
+        reporter('Tags do not match: %s and %s' % (x1.tag, x2.tag))
+        return False
+    for name, value in x1.attrib.items():
+        if x2.attrib.get(name) != value:
+            reporter('Attributes do not match: %s=%r, %s=%r'
+                     % (name, value, name, x2.attrib.get(name)))
+            return False
+    for name in x2.attrib.keys():
+        if name not in x1.attrib:
+            reporter('x2 has an attribute x1 is missing: %s'
+                     % name)
+            return False
+    if not _compareXmlText(x1.text, x2.text):
+        reporter('text: %r != %r' % (x1.text, x2.text))
+        return False
+    if not _compareXmlText(x1.tail, x2.tail):
+        reporter('tail: %r != %r' % (x1.tail, x2.tail))
+        return False
+    cl1 = x1.getchildren()
+    cl2 = x2.getchildren()
+    if len(cl1) != len(cl2):
+        reporter('children length differs, %i != %i'
+                 % (len(cl1), len(cl2)))
+        return False
+    i = 0
+    for c1, c2 in zip(cl1, cl2):
+        i += 1
+        if not compareXml(c1, c2, reporter=reporter):
+            reporter('children %i do not match: %s'
+                     % (i, c1.tag))
+            return False
+    return True
+
+
+def _compareXmlText(t1, t2):
+    if not t1 and not t2:
+        return True
+    if t1 == '*' or t2 == '*':
+        return True
+    return (t1 or '').strip() == (t2 or '').strip()
+
+
+def assertXmlEqual(a, b):
+    if isinstance(a, basestring):
+        a = _elementtree.fromstring(a)
+    if isinstance(b, basestring):
+        b = _elementtree.fromstring(b)
+    diffs = []
+    if not compareXml(a, b, diffs.append):
+        print('XMLs not equal.')
+        print('Diffs:', '\n'.join(diffs))
+        print('a:', a)
+        print('b:', b)
+        raise AssertionError('XMLs not equal.')
 
 
 def assertFoldersEqual(
