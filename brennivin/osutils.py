5a1b56df5ceebdf8a0f03250c82e5e6b41c35b53
Rob Galanakis <rob.galanakis@gmail.com>
2014-06-30 15:12:24 -0500

    Added osutils tests.

--- brennivin/osutils.py
+++ brennivin/osutils.py
@@ -7,11 +7,103 @@ Members
 """
 
 import binascii as _binascii
+import contextlib as _contextlib
 import errno as _errno
 import fnmatch as _fnmatch
 import os as _os
+import shutil as _shutil
 import stat as _stat
 import tempfile as _tempfile
+import threading as _threading
+
+altsep = _os.altsep
+if altsep is None:
+    altsep = _os.sep
+
+
+def abspathex(path, relative_to, _ignore_this=False):
+    """Returns a normalized absoluted version of the pathname ``path``,
+    relative to ``relativeTo`` directory.
+    ``relativeTo`` must be an actual directory.
+
+    :param path: The relative path to make absolute.
+    :param relative_to: The filename to make path absolute to.
+      This path will be made absolute before using it.
+    :param _ignore_this: For internal use only.
+
+    NOTE: This actually works by changing the cwd temporarily,
+    but will always set it back.
+    That said, there could be some side effects so use with care.
+    """
+    absRelativeTo = _os.path.abspath(relative_to)
+    with change_cwd(absRelativeTo):
+        if _ignore_this:
+            raise ArithmeticError
+        return _os.path.abspath(path)
+
+
+_changecwd_lock = _threading.Lock()
+
+
+@_contextlib.contextmanager
+def change_cwd(cwd):
+    """Context manager for temporarily changing the cwd.
+
+    :param cwd: The directory to use as the cwd.
+    """
+    orig = None
+    _changecwd_lock.acquire()
+    try:
+        orig = _os.getcwd()
+        _os.chdir(cwd)
+        yield
+    finally:
+        if orig is not None:
+            _os.chdir(orig)
+        _changecwd_lock.release()
+
+
+@_contextlib.contextmanager
+def change_environ(key, newvalue):
+    """Context manager for temporarily changing an os.environ entry.
+    A ``newvalue`` of None will delete the entry.
+    """
+    oldvalue = _os.environ.get(key, None)
+    if newvalue is None:
+        _os.environ.pop(key, None)
+    else:
+        _os.environ[key] = newvalue
+    try:
+        yield
+    finally:
+        if oldvalue is None:
+            _os.environ.pop(key, None)
+        else:
+            _os.environ[key] = oldvalue
+
+
+def change_ext(path, ext):
+    """Changes the extension of path to be ext.
+    If path has no extension, ext will be appended.
+
+    :param path: The path.
+    :param ext: The extension, should begin in a '.'.
+    """
+    root, ext_ = _os.path.splitext(path)
+    ext_ = ext
+    return root + ext_
+
+
+def copy(src, dst):
+    """Copies src to dst,
+    recursively making directories for dst if they do not exist.
+
+    src and dst should be filenames (not directories).
+    """
+    dirname = _os.path.dirname(dst)
+    if not _os.path.isdir(dirname):
+        _os.makedirs(dirname)
+    _shutil.copy(src, dst)
 
 
 def crc_from_filename(filename):
@@ -59,10 +151,30 @@ def mktemp(*args, **kwargs):
     return filename
 
 
+def path_components(path):
+    """Return list of a path's components."""
+    folders = path.replace(altsep, _os.sep).split(_os.sep)
+    result = [f for f in folders if f]
+    if _os.name != 'nt' and path[0] == '/':
+        result.insert(0, '/')
+    return result
+
+
+def purename(filename):
+    """Returns the basename of a path without the extension."""
+    if filename is None:
+        raise TypeError('filename cannot be None.')
+    f = _os.path.basename(filename)
+    return _os.path.splitext(f)[0]
+
+
 def set_readonly(path, state):
-    if state:
-        # Make it read-only
-        _os.chmod(path, _stat.S_IREAD)
-    else:
-        # Make it writeable
-        _os.chmod(path, _stat.S_IWRITE)
+    mode = _stat.S_IREAD if state else _stat.S_IWRITE
+    _os.chmod(path, mode)
+
+
+def split3(path):
+    """Returns a tuple of (dirname, filename without ext, ext)."""
+    dirname, filename = _os.path.split(path)
+    filename, ext = _os.path.splitext(filename)
+    return dirname, filename, ext
--- tests/test_osutils.py
+++ tests/test_osutils.py
@@ -1,5 +1,6 @@
 import inspect
 import os
+from os.path import join
 import shutil
 import stat
 import tempfile
@@ -7,7 +8,168 @@ import unittest
 
 from brennivin import osutils
 
+ROOT = '/'
+if os.name == 'nt':
+    ROOT = 'C:\\'
+
 THISDIR = os.path.dirname(os.path.abspath(__file__))
+
+
+class TestAbsPathEx(unittest.TestCase):
+    def testSuccess(self):
+        """Test result against some known values."""
+        result = osutils.abspathex('foo', ROOT)
+        self.assertEqual(result, join(ROOT, 'foo'))
+
+    def testNoneRaises(self):
+        """Test calling with None for the path."""
+        self.assertRaises(OSError, osutils.abspathex, None, 'foo')
+
+    def testRevertsCwdIfRaises(self):
+        """Ensures that the cwd is set back to what it was
+        even if the code raises.
+        Ensure that 'relative' is a valid path.
+        """
+        cwd = os.getcwd()
+        self.assertRaises(ArithmeticError, osutils.abspathex, 'path.py', ROOT, True)
+        self.assertEqual(cwd, os.getcwd())
+
+
+class ChangeCwdTests(unittest.TestCase):
+    def setUp(self):
+        self.oldcwd = os.getcwd()
+
+    def _changeAndTest(self, doRaise=False):
+        with osutils.change_cwd(THISDIR):
+            self.assertEqual(os.getcwd(), THISDIR)
+            if doRaise:
+                raise SystemError
+
+    def testChangesItBack(self):
+        """Test that the cwd is changed back."""
+        self._changeAndTest()
+        self.assertEqual(self.oldcwd, os.getcwd())
+
+    def testChangesWithError(self):
+        """Tests that the cwd is changed back after an error."""
+        try:
+            self._changeAndTest(True)
+        except SystemError:
+            self.assertEqual(self.oldcwd, os.getcwd())
+
+
+class TestChangeEnviron(unittest.TestCase):
+    def setUp(self):
+        os.environ['TESTVALUE'] = 'ABC'
+
+    def _changeAndTest(self, doRaise=False):
+        with osutils.change_environ('TESTVALUE', 'XYZ'):
+            self.assertEqual(os.environ['TESTVALUE'], 'XYZ')
+            if doRaise:
+                raise SystemError
+
+    def testChangesItBack(self):
+        """Tests that the value is changed back."""
+        self._changeAndTest()
+        self.assertEqual(os.environ['TESTVALUE'], 'ABC')
+
+    def testChangesWithError(self):
+        """Test that the value is changed back on an error."""
+        try:
+            self._changeAndTest(True)
+        except SystemError:
+            self.assertEqual(os.environ['TESTVALUE'], 'ABC')
+
+    def testDeletesIfItIsNew(self):
+        self.assertNotIn('blah', os.environ)
+        with osutils.change_environ('blah', 'f'):
+            self.assertEqual(os.environ['blah'], 'f')
+        self.assertNotIn('blah', os.environ)
+
+    def testReentrancy(self):
+        # Will lock if not reentrant
+        with osutils.change_environ('blah', 'f'):
+            with osutils.change_environ('spam', 'e'):
+                pass
+
+
+class ChangeExtTests(unittest.TestCase):
+    """All tests here should include tests on various extension lengths.  This isn't required but would be complete.
+        3 char with 3 char
+        1 char with 3 char
+        3 char with 1 char
+        1 char with 1 char
+        5 char with 3 char
+        3 char with 5 char
+        5 char with 5 char
+    """
+    def testWorksOnRelative(self):
+        """Test that relative paths return proper value.
+        """
+        self.assertEqual(osutils.change_ext('spam\\eggs.txt', '.ham'), 'spam\\eggs.ham')
+        self.assertEqual(osutils.change_ext('spam\\eggs.t', '.ham'), 'spam\\eggs.ham')
+        self.assertEqual(osutils.change_ext('spam\\eggs.txt', '.h'), 'spam\\eggs.h')
+        self.assertEqual(osutils.change_ext('spam\\eggs.t', '.h'), 'spam\\eggs.h')
+        self.assertEqual(osutils.change_ext('spam\\eggs.texts', '.ham'), 'spam\\eggs.ham')
+        self.assertEqual(osutils.change_ext('spam\\eggs.txt', '.hammm'), 'spam\\eggs.hammm')
+        self.assertEqual(osutils.change_ext('spam\\eggs.texts', '.hammm'), 'spam\\eggs.hammm')
+
+    def testWorksOnAbsolute(self):
+        """Test that absolute paths return proper value.
+        """
+        self.assertEqual(osutils.change_ext('C:\\spam\\eggs.txt', '.ham'), 'C:\\spam\\eggs.ham')
+        self.assertEqual(osutils.change_ext('C:\\spam\\eggs.t', '.ham'), 'C:\\spam\\eggs.ham')
+        self.assertEqual(osutils.change_ext('C:\\spam\\eggs.txt', '.h'), 'C:\\spam\\eggs.h')
+        self.assertEqual(osutils.change_ext('C:\\spam\\eggs.t', '.h'), 'C:\\spam\\eggs.h')
+        self.assertEqual(osutils.change_ext('C:\\spam\\eggs.texts', '.ham'), 'C:\\spam\\eggs.ham')
+        self.assertEqual(osutils.change_ext('C:\\spam\\eggs.txt', '.hammm'), 'C:\\spam\\eggs.hammm')
+        self.assertEqual(osutils.change_ext('C:\\spam\\eggs.texts', '.hammm'), 'C:\\spam\\eggs.hammm')
+
+    def testWorksOnFileOnly(self):
+        """Test that changeExt works with only a filename."""
+        self.assertEqual(osutils.change_ext('eggs.txt', '.ham'), 'eggs.ham')
+        self.assertEqual(osutils.change_ext('eggs.t', '.ham'), 'eggs.ham')
+        self.assertEqual(osutils.change_ext('eggs.txt', '.h'), 'eggs.h')
+        self.assertEqual(osutils.change_ext('eggs.t', '.h'), 'eggs.h')
+        self.assertEqual(osutils.change_ext('eggs.texts', '.ham'), 'eggs.ham')
+        self.assertEqual(osutils.change_ext('eggs.txt', '.hammm'), 'eggs.hammm')
+        self.assertEqual(osutils.change_ext('eggs.texts', '.hammm'), 'eggs.hammm')
+
+    def testAppendsExt(self):
+        """Test that the extension is appended if there is none."""
+        self.assertEqual(osutils.change_ext('eggs', '.txt'), 'eggs.txt')
+        self.assertEqual(osutils.change_ext('spam\\eggs', '.ham'), 'spam\\eggs.ham')
+
+    def testMultipleDotsWork(self):
+        """Test that paths with multiple periods work."""
+        self.assertEqual(osutils.change_ext('eggs.spam.foo', '.bar'), 'eggs.spam.bar')
+        self.assertEqual(osutils.change_ext('spam\\spam.spam.spam', '.bar'), 'spam\\spam.spam.bar')
+
+
+class TestCopy(unittest.TestCase):
+    def getRandomTempPath(self, tail=None):
+        result = join(tempfile.mkdtemp(), 'a', 'b', tail or 'tail')
+        self.assertFalse(os.path.isdir(result) or os.path.isfile(result))
+        return result
+
+    def makeTempFile(self):
+        fd, name = tempfile.mkstemp()
+        with os.fdopen(fd, 'w') as f:
+            f.write('foo')
+        return name
+
+    def testCopyMakesDirs(self):
+        """Test that copy makes dirs recursively for src."""
+        path = self.getRandomTempPath('spam.eggs')
+
+        osutils.copy(self.makeTempFile(), path)
+        self.assertTrue(os.path.isfile(path))
+
+    def testCopyRaisesIOErrorIfSrcDoesNotExist(self):
+        """Test that copy raises an IO error if src does not exist (same as 'copy' contract)."""
+        src = self.getRandomTempPath('spam.eggs2')
+        self.assertFalse(os.path.isfile(src))
+        self.assertRaises(IOError, lambda: osutils.copy(src, self.getRandomTempPath('spam.eggs')))
 
 
 class CrcFromFilenameTests(unittest.TestCase):
@@ -34,7 +196,7 @@ class IterFilesTests(unittest.TestCase):
             self.assertTrue(f.endswith('.py'))
 
     def testCanFindThisFile(self):
-        oneup = os.path.join(THISDIR, '..')
+        oneup = join(THISDIR, '..')
         files = [os.path.abspath(f) for f in osutils.iter_files(oneup, '*.py')]
         thispy = os.path.abspath(__file__).replace('.pyc', '.py')
         self.assertTrue(thispy in files)
@@ -48,7 +210,7 @@ class ListDirExTests(unittest.TestCase):
 
     def testDoesNotLookRecursively(self):
         """Test that listdirex on a path one dir up does not find this file."""
-        oneup = os.path.join(THISDIR, '/')
+        oneup = join(THISDIR, '/')
         files = [os.path.abspath(f) for f in osutils.listdirex(oneup, '*.py')]
         thispy = os.path.abspath(__file__).replace('.pyc', '.py')
         self.assertFalse(thispy in files)
@@ -68,13 +230,76 @@ class MakeDirsTests(unittest.TestCase):
     def testMakesDirIfNotExist(self):
         tempd = tempfile.mkdtemp()
         self.addCleanup(shutil.rmtree, tempd)
-        newd = os.path.join(tempd, 'a', 'b')
+        newd = join(tempd, 'a', 'b')
         self.assertFalse(os.path.isdir(newd))
         osutils.makedirs(newd)
         self.assertTrue(os.path.isdir(newd))
 
 
-class TestReadOnly(unittest.TestCase):
+class TestPathComponents(unittest.TestCase):
+    def assertComponentsEqual(self, path, ideal):
+        result = osutils.path_components(path)
+        self.assertEqual(result, ideal)
+
+    def testValidPaths(self):
+        self.assertComponentsEqual(join(ROOT, 'foo', 'bar.baz'),
+                                   [ROOT, 'foo', 'bar.baz'])
+        self.assertComponentsEqual(join('foo', 'bar.baz'),
+                                   ['foo', 'bar.baz'])
+
+    def testForwardSlashes(self):
+        self.assertComponentsEqual('c:/foo/bar.baz',
+                                   ['c:', 'foo', 'bar.baz'])
+        self.assertComponentsEqual('foo/bar.baz',
+                                   ['foo', 'bar.baz'])
+
+    def testRespaths(self):
+        self.assertComponentsEqual('res:/foo/bar.baz',
+                                   ['res:', 'foo', 'bar.baz'])
+
+
+class PurenameTests(unittest.TestCase):
+    def testFailsForNone(self):
+        self.assertRaises(TypeError, osutils.purename, None)
+
+    def testEmptyPath(self):
+        self.assertEqual('', osutils.purename(''))
+
+    def testRelative(self):
+        self.assertEqual('foo', osutils.purename(join('blah', 'foo.text')))
+
+    def testAbsolute(self):
+        """Test that method works with absolute paths."""
+        self.assertEqual('spam', osutils.purename(os.path.abspath(join('eggs', 'spam.h'))))
+
+    def testFilenameOnly(self):
+        """Test that method works when passed a path-less filename."""
+        self.assertEqual('eggs', osutils.purename('eggs.txt'))
+
+    def testFilenameWithoutExtension(self):
+        """Test that method returns the passed in value if it is just a plan string."""
+        self.assertEqual('spam', osutils.purename('spam'))
+
+    def testRes(self):
+        self.assertEqual(
+            'bar', osutils.purename('res:/foo/bar.red'))
+
+    def testReturnsExtensionIfNoFilename(self):
+        """Test that method returns the 'extension' if that is the only part of the filename.  I think this is standard
+        filename behavior.
+        """
+        self.assertEqual('.txt', osutils.purename('.txt'))
+
+    def testReturnsFilenameIfStartsWithDot(self):
+        """Test that method returns the proper filename even if the path passed starts with a dot."""
+        self.assertEqual('.spam.eggs', osutils.purename('.spam.eggs.ham'))
+
+    def testExtractsOnlyLastIfMultipleDots(self):
+        """Test that the actual extension is removed if the file has multiple dots."""
+        self.assertEqual('spam.eggs', osutils.purename(join('foo', 'spam.eggs.bar')))
+
+
+class SetReadOnlyTests(unittest.TestCase):
 
     def setUp(self):
         self.writeable = osutils.mktemp()
@@ -97,3 +322,32 @@ class TestReadOnly(unittest.TestCase):
         """Test that a readonly file can be made writeable."""
         osutils.set_readonly(self.readonly, False)
         self.assertTrue(os.access(self.readonly, os.W_OK))
+
+
+class TestSplitPath(unittest.TestCase):
+    def assertSplit(self, arg, ideal):
+        got = osutils.split3(arg)
+        self.assertEqual(got, ideal)
+
+    def testAbsPaths(self):
+        self.assertSplit(join(ROOT, 'foo', 'spam', 'eggs.ham'),
+                         (join(ROOT, 'foo', 'spam'), 'eggs', '.ham'))
+        self.assertSplit(join(ROOT, 'foo.ham'),
+                         (ROOT, 'foo', '.ham'))
+
+    def testRelativePaths(self):
+        self.assertSplit(join('foo', 'ham.eggs'),
+                         ('foo', 'ham', '.eggs'))
+        self.assertSplit(join(ROOT, 'foo', '..', 'bar', 'eggs.ham'),
+                         (join(ROOT, 'foo', '..', 'bar'), 'eggs', '.ham'))
+        self.assertSplit(join('foo', '..', 'ham.eggs'),
+                         (join('foo', '..'), 'ham', '.eggs'))
+
+    def testNoDir(self):
+        self.assertSplit('foo.ham', ('', 'foo', '.ham'))
+
+    def testNoExt(self):
+        self.assertSplit(join('bar', 'foo'), ('bar', 'foo', ''))
+
+    def testEmpty(self):
+        self.assertSplit('', ('', '', ''))
